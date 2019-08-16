c40a0db82da53014c5a5e2b9902afcf5f0ddf0d3
Joel Aquilina <loadedice@users.noreply.github.com>
2019-08-16 11:05:20 +1000

    Fix most flake8 issues

--- src/example/settings.py
+++ src/example/settings.py
@@ -87,18 +87,19 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
+validator_prefix = 'django.contrib.auth.password_validation.'
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': f'{validator_prefix}UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': f'{validator_prefix}.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': f'{validator_prefix}.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': f'{validator_prefix}.NumericPasswordValidator',
     },
 ]
 
