b68a74780ad102a50784439b4876d09dd8271d04
Joel Aquilina <loadedice@users.noreply.github.com>
2019-08-20 20:15:29 +1000

    Read DEBUG and SECURITY KEY from dot env file

--- src/credo/settings.py
+++ src/credo/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xbjxvyi9+jj&o)t$#qp_!%2zi@!+6!#9b@pow-6pyj_m9fk#np'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DJANGO_DEBUG'] == 'true'
+
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
