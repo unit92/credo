05519ea4f01ffe27ab53d1139c3c3539f07d82ed
Kpaiy <kpaiy42@gmail.com>
2019-09-16 20:58:55 +1000

    Render comment SVGs where appropriate
    They could certainly use some positioning cleanup, though.

--- src/credo/static/credo/credo-toolkit.js
+++ src/credo/static/credo/credo-toolkit.js
@@ -17,7 +17,10 @@ class CredoToolkit {
   verovioToolkit
 
   commentSvg = `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM18 14H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="480" height="480" viewBox="0 0 24 24">
+      <path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM18 14H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z" fill="green" />
+      <path d="M0 0h24v24H0z" fill="none"/>
+    </svg>
  `
 
   /**
@@ -130,10 +133,18 @@ class CredoToolkit {
   }
 
   renderComments () {
-    // TODO
+    this.comments.forEach(this.renderComment.bind(this))
   }
 
   renderComment (comment) {
+    // get the element to which we want to attack the comment
+    const element = document.getElementById(comment.position)
+    const position = element.getBBox()
 
+    element.innerHTML += this.commentSvg
+
+    const commentElement = element.children[(element.children.length - 1)]
+    commentElement.setAttribute('x', position.x)
+    commentElement.setAttribute('y', position.y)
   }
 }
