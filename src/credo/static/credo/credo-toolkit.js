7578b7f33fb3088d1c60a1944fe77a01978c3848
Kpaiy <kpaiy42@gmail.com>
2019-09-17 09:51:40 +1000

    Remove unused .svg file
    We had imported the SVG into the JavaScript, so we didn't need the file.

--- src/credo/static/credo/credo-toolkit.js
+++ src/credo/static/credo/credo-toolkit.js
@@ -16,6 +16,7 @@ class CredoToolkit {
 
   verovioToolkit
 
+  // Sourced from https://material.io/resources/icons/
   commentSvg = `
     <svg xmlns="http://www.w3.org/2000/svg" width="480" height="480" viewBox="0 0 24 24">
       <path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM18 14H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z" fill="green" />
