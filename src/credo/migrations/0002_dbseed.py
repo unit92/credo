da31651503e2b49d274020586a372fa6d01805e4
Laith Majeed <lmaj3891@uni.sydney.edu.au>
2019-09-17 20:43:28 +1000

    Fix code so that adding additional records from Django admin site interface does not cause an error
    This originally occured because I specified a primary key id per record. If you try to insert a record
    via admin site with a manual key, Django returns an error stating the key already exists (due to manual pk insertion).
    Strangely enough if you keep attempting to insert via the site, it increments the pk key it attempts to insert in.
    
    - Condensed code
    - Adjusted delete conditions to look for the name of a composer, song, etc. instead of an arbitray pk.

--- src/credo/migrations/0002_dbseed.py
+++ src/credo/migrations/0002_dbseed.py
@@ -9,16 +9,16 @@ def load_composers(apps, schema_editor):
     Composer = apps.get_model('credo', 'Composer')
 
     time = timezone.now()
-    c1 = Composer(id=0, name='Craig Smith', created_at=time, updated_at=time)
+    c1 = Composer(name='Craig Smith')
     c1.save()
 
-    c2 = Composer(id=1, name='Luke Tuthill', created_at=time, updated_at=time)
+    c2 = Composer(name='Luke Tuthill')
     c2.save()
 
-    c3 = Composer(id=2, name='Joel Aquilina', created_at=time, updated_at=time)
+    c3 = Composer(name='Joel Aquilina')
     c3.save()
 
-    c4 = Composer(id=3, name='Alex Mirrington', created_at=time, updated_at=time)
+    c4 = Composer(name='Alex Mirrington')
     c4.save()
 
 def load_songs(apps, schema_editor):
@@ -26,24 +26,20 @@ def load_songs(apps, schema_editor):
     Composer = apps.get_model('credo', 'Composer')
 
     time = timezone.now()
-    composer = Composer.objects.get(id=0)
-    s1 = Song(id=0, name='JS Standard', composer=composer, created_at=time,
-            updated_at=time)
+    composer = Composer.objects.get(name='Craig Smith')
+    s1 = Song(name='JS Standard', composer=composer)
     s1.save()
 
-    composer = Composer.objects.get(id=1)
-    s2 = Song(id=1, name='Jingle Django', composer=composer, created_at=time,
-            updated_at=time)
+    composer = Composer.objects.get(name='Luke Tuthill')
+    s2 = Song(name='Jingle Django', composer=composer)
     s2.save()
 
-    composer = Composer.objects.get(id=2)
-    s3 = Song(id=2, name='Environment Song', composer=composer, created_at=time,
-            updated_at=time)
+    composer = Composer.objects.get(name='Joel Aquilina')
+    s3 = Song(name='Environment Song', composer=composer)
     s3.save()
 
-    composer = Composer.objects.get(id=3)
-    s4 = Song(id=3, name='Diffing Song', composer=composer, created_at=time,
-            updated_at=time)
+    composer = Composer.objects.get(name='Alex Mirrington')
+    s4 = Song(name='Diffing Song', composer=composer)
     s4.save()
 
 
@@ -51,38 +47,38 @@ def load_mei(apps, schema_editor):
     MEI = apps.get_model('credo', 'MEI')
 
     time = timezone.now()
-    m1 = MEI(id=0, data='./credo/migrations/seed_mei/SourceA.mei', created_at=time,
-            updated_at=time)
+    path = './credo/migrations/seed_mei/'
+    m1 = MEI(data=path + 'SourceA.mei')
     m1.save()
 
-    m2 = MEI(id=1, data='./credo/migrations/seed_mei/SourceB.mei', created_at=time,
-            updated_at=time)
+    m2 = MEI(data=path + 'SourceB.mei')
     m2.save()
 
-    m3 = MEI(id=2, data='./credo/migrations/seed_mei/SourceC.mei', created_at=time,
-            updated_at=time)
+    m3 = MEI(data=path + 'SourceC.mei')
     m3.save()
 
 #ROLLBACK FUNCTIONS
 def delete_composers(apps, schema_editor):
     Composer = apps.get_model('credo', 'Composer')
-    Composer.objects.get(id=0).delete()
-    Composer.objects.get(id=1).delete()
-    Composer.objects.get(id=2).delete()
-    Composer.objects.get(id=3).delete()
+    Composer.objects.get(name='Craig Smith').delete()
+    Composer.objects.get(name='Luke Tuthill').delete()
+    Composer.objects.get(name='Joel Aquilina').delete()
+    Composer.objects.get(name='Alex Mirrington').delete()
 
 def delete_songs(apps, schema_editor):
     Song = apps.get_model('credo', 'Song')
-    Song.objects.get(id=0).delete()
-    Song.objects.get(id=1).delete()
-    Song.objects.get(id=2).delete()
-    Song.objects.get(id=3).delete()
+    Song.objects.get(name='JS Standard').delete()
+    Song.objects.get(name='Jingle Django').delete()
+    Song.objects.get(name='Environment Song').delete()
+    Song.objects.get(name='Diffing Song').delete()
+    # Song.objects.all().delete()
 
 def delete_mei(apps, schema_editor):
     MEI = apps.get_model('credo', 'MEI')
-    MEI.objects.get(id=0).delete()
-    MEI.objects.get(id=1).delete()
-    MEI.objects.get(id=2).delete()
+    path = './credo/migrations/seed_mei/'
+    MEI.objects.get(data=path + 'SourceA.mei').delete()
+    MEI.objects.get(data=path + 'SourceB.mei').delete()
+    MEI.objects.get(data=path + 'SourceC.mei').delete()
 
 class Migration(migrations.Migration):
 
