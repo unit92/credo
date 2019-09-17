033206301fbd69a3e928d084a5ec3d7498f78321
Alex Mirrington <alexmirrington@gmail.com>
2019-09-17 19:36:03 +1000

    Implement comparison methods

--- src/credo/utils/mei/comparison_strategy.py
+++ src/credo/utils/mei/comparison_strategy.py
@@ -1,4 +1,24 @@
-class ComparisonStrategy:
+from abc import ABC, abstractmethod
+import lxml.etree as et
+from credo.models import MEI
+
+
+class ComparisonStrategy(ABC):
 
     def __init__(self):
         return
+
+    def compare_meis(self, a: MEI, b: MEI) -> tuple:
+
+        # TODO Exception handling
+        with a.data.open() as f:
+            tree_a = et.parse(f)
+
+        with b.data.open() as f:
+            tree_b = et.parse(f)
+
+        return self.compare_trees(tree_a, tree_b)
+
+    @abstractmethod
+    def compare_trees(self, a: et.ElementTree, b: et.ElementTree) -> tuple:
+        pass
--- src/credo/utils/mei/tree_comparison.py
+++ src/credo/utils/mei/tree_comparison.py
@@ -1,7 +1,21 @@
 from .comparison_strategy import ComparisonStrategy
+import lxml.etree as et
 
 
 class TreeComparison(ComparisonStrategy):
 
     def __init__(self):
         return
+
+    def compare_trees(self, a: et.ElementTree, b: et.ElementTree) -> tuple:
+
+        a = self.strip_tree(a)
+        b = self.strip_tree(b)
+
+        # TODO actual diff algorithm here
+        diff = a
+        return diff, a, b
+
+    def strip_tree(self, t: et.ElementTree) -> et.ElementTree:
+        # TODO Call Joel's intermediate MEI format functions here
+        return t
