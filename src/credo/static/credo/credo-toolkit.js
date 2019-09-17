2be5e3baf98d68f79f4db96d2a270324ff132a60
Kpaiy <kpaiy42@gmail.com>
2019-09-17 10:27:46 +1000

    Fix bug with fill
    Changed background fill from "none" to "transparent." "Why?" I hear you
    ask. It's because the tooltip won't be triggered on the transparent
    parts of the comment unless this is applied. Go figure.

--- src/credo/static/credo/credo-toolkit.js
+++ src/credo/static/credo/credo-toolkit.js
@@ -20,7 +20,7 @@ class CredoToolkit {
   commentSvg = `
     <svg class="tooltipped" data-position="bottom" xmlns="http://www.w3.org/2000/svg" width="480" height="480" viewBox="0 0 24 24">
       <path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM18 14H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z" fill="green" />
-      <path d="M0 0h24v24H0z" fill="none"/>
+      <path d="M0 0h24v24H0z" fill="transparent"/>
     </svg>
  `
 
