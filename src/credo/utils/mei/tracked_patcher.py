c30bd5b3dfd3109e8315faf0abd2b7bb1f3b4fe7
Alex Mirrington <alexmirrington@gmail.com>
2019-09-22 23:19:05 +1000

    Implement basic action tracking

--- src/credo/utils/mei/tracked_patcher.py
+++ src/credo/utils/mei/tracked_patcher.py
@@ -1,45 +1,108 @@
+from lxml import etree
 from xmldiff.patch import Patcher
+from copy import deepcopy
 
 
 class TrackedPatcher(Patcher):
-    def __init__(self):
-        return
 
-    def patch(self, actions, tree):
-        super().patch(actions, tree)
+    class ModifiedNode:
+        def __init__(self, node):
+            self.node = node
+            self.modifications = []
+
+        def register_modification(self, action):
+            self.modifications.append(action)
+
+    def __init__(self):
+        self.modfications = []
+
+    def patch(self, actions, tree, copy=True):
+        result = deepcopy(tree) if copy else tree
+
+        for action in actions:
+            print(action)
+            self.handle_action(action, result)
+
+        for mod in self.modfications:
+            print(mod.node, mod.modifications)
+
+        return result
+
+    def register_action(self, action, tree, node):
+        modified_nodes = [x.node for x in self.modfications]
+        if node in modified_nodes:
+            i = modified_nodes.index(node)
+            self.modfications[i].register_modification(action)
+        else:
+            modified_node = TrackedPatcher.ModifiedNode(node)
+            modified_node.register_modification(action)
+            self.modfications.append(modified_node)
 
     def handle_action(self, action, tree):
-        super.handle_action(action, tree)
+        super().handle_action(action, tree)
 
     def _handle_DeleteNode(self, action, tree):
-        super()._handle_DeleteNode(action, tree)
+        node = tree.xpath(action.node)[0]
+        node.getparent().remove(node)
+        self.register_action(action, tree, node)
 
     def _handle_InsertNode(self, action, tree):
-        super()._handle_InsertNode(action, tree)
+        target = tree.xpath(action.target)[0]
+        node = target.makeelement(action.tag)
+        target.insert(action.position, node)
+        self.register_action(action, tree, node)
 
     def _handle_RenameNode(self, action, tree):
-        super()._handle_RenameNode(action, tree)
+        node = tree.xpath(action.node)[0]
+        node.tag = action.tag
+        self.register_action(action, tree, node)
 
     def _handle_MoveNode(self, action, tree):
-        super()._handle_MoveNode(action, tree)
+        node = tree.xpath(action.node)[0]
+        node.getparent().remove(node)
+        target = tree.xpath(action.target)[0]
+        target.insert(action.position, node)
+        self.register_action(action, tree, node)
 
     def _handle_UpdateTextIn(self, action, tree):
-        super()._handle_UpdateTextIn(action, tree)
+        node = tree.xpath(action.node)[0]
+        node.text = action.text
+        self.register_action(action, tree, node)
 
     def _handle_UpdateTextAfter(self, action, tree):
-        super()._handle_UpdateTextAfter(action, tree)
+        node = tree.xpath(action.node)[0]
+        node.tail = action.text
+        self.register_action(action, tree, node)
 
     def _handle_UpdateAttrib(self, action, tree):
-        super()._handle_UpdateAttrib(action, tree)
+        node = tree.xpath(action.node)[0]
+        # This should not be used to insert new attributes.
+        assert action.name in node.attrib
+        node.attrib[action.name] = action.value
+        self.register_action(action, tree, node)
 
     def _handle_DeleteAttrib(self, action, tree):
-        super()._handle_DeleteAttrib(action, tree)
+        node = tree.xpath(action.node)[0]
+        del node.attrib[action.name]
+        self.register_action(action, tree, node)
 
     def _handle_InsertAttrib(self, action, tree):
-        super()._handle_InsertAttrib(action, tree)
+        node = tree.xpath(action.node)[0]
+        # This should not be used to update existing attributes.
+        assert action.name not in node.attrib
+        node.attrib[action.name] = action.value
+        self.register_action(action, tree, node)
 
     def _handle_RenameAttrib(self, action, tree):
-        super()._handle_RenameAttrib(action, tree)
+        node = tree.xpath(action.node)[0]
+        assert action.oldname in node.attrib
+        assert action.newname not in node.attrib
+        node.attrib[action.newname] = node.attrib[action.oldname]
+        del node.attrib[action.oldname]
+        self.register_action(action, tree, node)
 
     def _handle_InsertComment(self, action, tree):
-        super()._handle_InsertComment(action, tree)
+        target = tree.xpath(action.target)[0]
+        node = etree.Comment(action.text)
+        target.insert(action.position, node)
+        self.register_action(action, tree, node)
--- src/credo/utils/mei/tree_comparison.py
+++ src/credo/utils/mei/tree_comparison.py
@@ -1,9 +1,9 @@
-from xmldiff import main, actions
+from xmldiff import main
 import lxml.etree as et
 import typing as t
-from copy import deepcopy
 
 from .comparison_strategy import ComparisonStrategy
+from .tracked_patcher import TrackedPatcher
 from utils.mei.mei_transformer import MeiTransformer
 
 
@@ -43,7 +43,7 @@ class TreeComparison(ComparisonStrategy):
         a_transformer.to_intermediate()
         b_transformer.to_intermediate()
 
-        diff = self.__get_diff(a_transformer.tree, b_transformer.tree)
+        diff = self.__get_diff_tree(a_transformer.tree, b_transformer.tree)
 
         diff_transformer = MeiTransformer(diff)
 
@@ -53,16 +53,7 @@ class TreeComparison(ComparisonStrategy):
 
         return diff_transformer.tree, a_transformer.tree, b_transformer.tree
 
-    def __get_diff(self, a: et.ElementTree, b: et.ElementTree, ):
-
-        # Define functions for handline different types of xmldiff actions
-        action_map = {
-            actions.UpdateAttrib: self.__process_update,
-            actions.DeleteAttrib: self.__process_update,
-            actions.InsertAttrib: self.__process_update,
-            actions.DeleteNode: self.__process_deletion
-        }
-
+    def __get_diff_tree(self, a: et.ElementTree, b: et.ElementTree, ):
         # Get a list of actions to apply to tree a that
         # when applied transform it into tree b
         diff_actions = main.diff_trees(
@@ -75,17 +66,8 @@ class TreeComparison(ComparisonStrategy):
             }
         )
 
-        diff = deepcopy(a)
-
-        for diff_action in diff_actions:
-            # Ignore all actions that are not supported by the
-            # comparison algorithm.
-            print(diff_action)
-            if type(diff_action) not in action_map:
-                continue
-
-            process_action = action_map.get(type(diff_action))
-            process_action(diff, a, b, diff_action)
+        patcher = TrackedPatcher()
+        diff = patcher.patch(diff_actions, a, copy=True)
 
         print('A:\n', et.tostring(a, pretty_print=True).decode(), '\n')
         print('B:\n', et.tostring(b, pretty_print=True).decode(), '\n')
@@ -93,191 +75,191 @@ class TreeComparison(ComparisonStrategy):
 
         return diff
 
-    def __get_diff_layers(self, diff: et.ElementTree, layer_id: str):
-        # Find the corresponding layer in the diff tree
-        diff_layer_query = et.XPath(
-            '//mei:layer[@xml:id={}]'.format(layer_id),
-            namespaces=TreeComparison.ns
-        )
-        diff_common_layer = diff_layer_query(diff)[0]
+    # def __get_diff_layers(self, diff: et.ElementTree, layer_id: str):
+    #     # Find the corresponding layer in the diff tree
+    #     diff_layer_query = et.XPath(
+    #         '//mei:layer[@xml:id={}]'.format(layer_id),
+    #         namespaces=TreeComparison.ns
+    #     )
+    #     diff_common_layer = diff_layer_query(diff)[0]
 
-        # Check for existing sibling layers describing content specific to
-        # the a and b trees
-        layer_qry_str = 'following-sibling::mei:layer[@xml:id=$layer_id]|\
-            preceding-sibling::mei:layer[@xml:id=$layer_id]'
+    #     # Check for existing sibling layers describing content specific to
+    #     # the a and b trees
+    #     layer_qry_str = 'following-sibling::mei:layer[@xml:id=$layer_id]|\
+    #         preceding-sibling::mei:layer[@xml:id=$layer_id]'
 
-        layer_id_suffixes = ['a', 'b']
+    #     layer_id_suffixes = ['a', 'b']
 
-        diff_a_layer = diff_common_layer.xpath(
-            layer_qry_str,
-            namespaces=TreeComparison.ns,
-            layer_id=layer_id + layer_id_suffixes[0]
-        )
-        diff_b_layer = diff_common_layer.xpath(
-            layer_qry_str,
-            namespaces=TreeComparison.ns,
-            layer_id=layer_id + layer_id_suffixes[1]
-        )
-        layers = [diff_a_layer, diff_b_layer]
+    #     diff_a_layer = diff_common_layer.xpath(
+    #         layer_qry_str,
+    #         namespaces=TreeComparison.ns,
+    #         layer_id=layer_id + layer_id_suffixes[0]
+    #     )
+    #     diff_b_layer = diff_common_layer.xpath(
+    #         layer_qry_str,
+    #         namespaces=TreeComparison.ns,
+    #         layer_id=layer_id + layer_id_suffixes[1]
+    #     )
+    #     layers = [diff_a_layer, diff_b_layer]
 
-        # Create layers for storing content specific to
-        # the a and b trees if not already created
-        XML_ID = et.QName(TreeComparison.ns['xml'], 'id')
+    #     # Create layers for storing content specific to
+    #     # the a and b trees if not already created
+    #     XML_ID = et.QName(TreeComparison.ns['xml'], 'id')
 
-        i = 0
-        for i in range(len(layers)):
-            if len(layers[i]) == 0:
-                layers[i] = deepcopy(diff_common_layer)
-                layers[i].set(
-                    XML_ID.text,
-                    '{}{}'.format(layer_id, layer_id_suffixes[i])
-                )
-                for elem in layers[i].iter():
-                    elem.set('visible', 'false')
-                diff_common_layer.getparent().append(layers[i])
-            else:
-                layers[i] = layers[i][0]
-            i += 1
-        layers.insert(0, diff_common_layer)
-        return layers
+    #     i = 0
+    #     for i in range(len(layers)):
+    #         if len(layers[i]) == 0:
+    #             layers[i] = deepcopy(diff_common_layer)
+    #             layers[i].set(
+    #                 XML_ID.text,
+    #                 '{}{}'.format(layer_id, layer_id_suffixes[i])
+    #             )
+    #             for elem in layers[i].iter():
+    #                 elem.set('visible', 'false')
+    #             diff_common_layer.getparent().append(layers[i])
+    #         else:
+    #             layers[i] = layers[i][0]
+    #         i += 1
+    #     layers.insert(0, diff_common_layer)
+    #     return layers
 
-    def __get_containing_layer(self, node: et.Element):
+    # def __get_containing_layer(self, node: et.Element):
 
-        layer_query = et.XPath(
-            'ancestor::mei:layer',
-            namespaces=TreeComparison.ns
-        )
-        # There will only ever be one containing layer per node in a valid MEI
-        containing_layer = layer_query(node)
-        return containing_layer[0]
+    #     layer_query = et.XPath(
+    #         'ancestor::mei:layer',
+    #         namespaces=TreeComparison.ns
+    #     )
+    #     #There will only ever be one containing layer per node in a valid MEI
+    #     containing_layer = layer_query(node)
+    #     return containing_layer[0]
 
-    def __pad_after_node(self, node: et.Element, duration: float):
-        parent = node.getparent()
-        if len(parent) == 0:
-            raise ValueError('Supplied node must have a parent element.')
-        idx = parent.index(node)
-        print(idx)
+    # def __pad_after_node(self, node: et.Element, duration: float):
+    #     parent = node.getparent()
+    #     if len(parent) == 0:
+    #         raise ValueError('Supplied node must have a parent element.')
+    #     idx = parent.index(node)
+    #     print(idx)
 
-        pad_elem = et.SubElement(parent, 'space')
-        pad_elem.set('dur', str(int(1/duration)))
+    #     pad_elem = et.SubElement(parent, 'space')
+    #     pad_elem.set('dur', str(int(1/duration)))
 
-        parent.insert(idx + 1, pad_elem)
+    #     parent.insert(idx + 1, pad_elem)
 
-    def __process_update(
-            self,
-            diff: et.ElementTree,
-            a: et.ElementTree,
-            b: et.ElementTree,
-            action: actions.UpdateAttrib,
-            a_color=A_COLOR,
-            b_color=B_COLOR):
+    # def __process_update(
+    #         self,
+    #         diff: et.ElementTree,
+    #         a: et.ElementTree,
+    #         b: et.ElementTree,
+    #         action: actions.UpdateAttrib,
+    #         a_color=A_COLOR,
+    #         b_color=B_COLOR):
 
-        # Define tags supported by this action
-        supported_tags = (et.QName(TreeComparison.ns['mei'], 'note'),)
-        supported_tags = [tag.text for tag in supported_tags]
+    #     # Define tags supported by this action
+    #     supported_tags = (et.QName(TreeComparison.ns['mei'], 'note'),)
+    #     supported_tags = [tag.text for tag in supported_tags]
 
-        node_query = et.XPath(action.node)
+    #     node_query = et.XPath(action.node)
 
-        XML_ID = et.QName(TreeComparison.ns['xml'], 'id')
+    #     XML_ID = et.QName(TreeComparison.ns['xml'], 'id')
 
-        # Query returns list, just want the first element (single node)
-        nodes = node_query(a)
-        if len(nodes) != 1:
-            return
-        node = nodes[0]
-        node_id = node.get(XML_ID.text)
+    #     # Query returns list, just want the first element (single node)
+    #     nodes = node_query(a)
+    #     if len(nodes) != 1:
+    #         return
+    #     node = nodes[0]
+    #     node_id = node.get(XML_ID.text)
 
-        if node.tag in supported_tags:
+    #     if node.tag in supported_tags:
 
-            containing_layer = self.__get_containing_layer(node)
-            containing_layer_id = containing_layer.get(XML_ID.text)
+    #         containing_layer = self.__get_containing_layer(node)
+    #         containing_layer_id = containing_layer.get(XML_ID.text)
 
-            layers = self.__get_diff_layers(diff, containing_layer_id)
-            common_layer, a_layer, b_layer = layers
+    #         layers = self.__get_diff_layers(diff, containing_layer_id)
+    #         common_layer, a_layer, b_layer = layers
 
-            diff_node_query = et.XPath(
-                'descendant::*[@xml:id={}]'.format(node_id),
-                namespaces=TreeComparison.ns
-            )
-            diff_common_node = diff_node_query(common_layer)[0]
-            diff_a_node = diff_node_query(a_layer)[0]
-            diff_b_node = diff_node_query(b_layer)[0]
+    #         diff_node_query = et.XPath(
+    #             'descendant::*[@xml:id={}]'.format(node_id),
+    #             namespaces=TreeComparison.ns
+    #         )
+    #         diff_common_node = diff_node_query(common_layer)[0]
+    #         diff_a_node = diff_node_query(a_layer)[0]
+    #         diff_b_node = diff_node_query(b_layer)[0]
 
-            # TODO Think about spacing when updated attribute
-            # is dur or dots attribute. Group actions?
+    #         # TODO Think about spacing when updated attribute
+    #         # is dur or dots attribute. Group actions?
 
-            # Apply attribute update to b
-            if isinstance(action, actions.DeleteAttrib):
-                diff_b_node.attrib.pop(action.name)
-            else:
-                diff_b_node.set(action.name, action.value)
+    #         # Apply attribute update to b
+    #         if isinstance(action, actions.DeleteAttrib):
+    #             diff_b_node.attrib.pop(action.name)
+    #         else:
+    #             diff_b_node.set(action.name, action.value)
 
-            # Change colors
-            if action.name != XML_ID:
-                # Update implies the change is not common to
-                # source a and b, hence make this node invisible.
-                diff_common_node.set('visible', 'false')
-                diff_a_node.set('color', a_color)
-                diff_a_node.set('visible', 'true')
-                diff_b_node.set('color', b_color)
-                diff_b_node.set('visible', 'true')
+    #         # Change colors
+    #         if action.name != XML_ID:
+    #             # Update implies the change is not common to
+    #             # source a and b, hence make this node invisible.
+    #             diff_common_node.set('visible', 'false')
+    #             diff_a_node.set('color', a_color)
+    #             diff_a_node.set('visible', 'true')
+    #             diff_b_node.set('color', b_color)
+    #             diff_b_node.set('visible', 'true')
 
-    def __process_insertion(
-            self,
-            diff: et.ElementTree,
-            a: et.ElementTree,
-            b: et.ElementTree,
-            action: actions.InsertNode,
-            a_color=A_COLOR,
-            b_color=B_COLOR):
-        return
+    # def __process_insertion(
+    #         self,
+    #         diff: et.ElementTree,
+    #         a: et.ElementTree,
+    #         b: et.ElementTree,
+    #         action: actions.InsertNode,
+    #         a_color=A_COLOR,
+    #         b_color=B_COLOR):
+    #     return
 
-    def __process_deletion(
-            self,
-            diff: et.ElementTree,
-            a: et.ElementTree,
-            b: et.ElementTree,
-            action: actions.DeleteNode,
-            a_color=A_COLOR,
-            b_color=B_COLOR):
+    # def __process_deletion(
+    #         self,
+    #         diff: et.ElementTree,
+    #         a: et.ElementTree,
+    #         b: et.ElementTree,
+    #         action: actions.DeleteNode,
+    #         a_color=A_COLOR,
+    #         b_color=B_COLOR):
 
-        # Define tags supported by this action
-        supported_tags = (et.QName(TreeComparison.ns['mei'], 'note'),)
-        supported_tags = [tag.text for tag in supported_tags]
+    #     # Define tags supported by this action
+    #     supported_tags = (et.QName(TreeComparison.ns['mei'], 'note'),)
+    #     supported_tags = [tag.text for tag in supported_tags]
 
-        node_query = et.XPath(action.node)
+    #     node_query = et.XPath(action.node)
 
-        XML_ID = et.QName(TreeComparison.ns['xml'], 'id')
+    #     XML_ID = et.QName(TreeComparison.ns['xml'], 'id')
 
-        # Query returns list, just want the first element (single node)
-        nodes = node_query(a)
-        if len(nodes) != 1:
-            return
-        node = nodes[0]
-        node_id = node.get(XML_ID.text)
+    #     # Query returns list, just want the first element (single node)
+    #     nodes = node_query(a)
+    #     if len(nodes) != 1:
+    #         return
+    #     node = nodes[0]
+    #     node_id = node.get(XML_ID.text)
 
-        if node.tag in supported_tags:
+    #     if node.tag in supported_tags:
 
-            containing_layer = self.__get_containing_layer(node)
-            containing_layer_id = containing_layer.get(XML_ID.text)
+    #         containing_layer = self.__get_containing_layer(node)
+    #         containing_layer_id = containing_layer.get(XML_ID.text)
 
-            layers = self.__get_diff_layers(diff, containing_layer_id)
-            common_layer, a_layer, b_layer = layers
+    #         layers = self.__get_diff_layers(diff, containing_layer_id)
+    #         common_layer, a_layer, b_layer = layers
 
-            diff_node_query = et.XPath(
-                'descendant::*[@xml:id={}]'.format(node_id),
-                namespaces=TreeComparison.ns
-            )
-            diff_common_node = diff_node_query(common_layer)[0]
-            diff_a_node = diff_node_query(a_layer)[0]
-            diff_b_node = diff_node_query(b_layer)[0]
+    #         diff_node_query = et.XPath(
+    #             'descendant::*[@xml:id={}]'.format(node_id),
+    #             namespaces=TreeComparison.ns
+    #         )
+    #         diff_common_node = diff_node_query(common_layer)[0]
+    #         diff_a_node = diff_node_query(a_layer)[0]
+    #         diff_b_node = diff_node_query(b_layer)[0]
 
-            # Update implies the change is not common to source a and b
-            # hence make this node invisible
-            diff_common_node.set('visible', 'false')
+    #         # Update implies the change is not common to source a and b
+    #         # hence make this node invisible
+    #         diff_common_node.set('visible', 'false')
 
-            diff_a_node.set('color', a_color)
-            diff_a_node.set('visible', 'true')
+    #         diff_a_node.set('color', a_color)
+    #         diff_a_node.set('visible', 'true')
 
-            # Apply update to b
-            b_layer.remove(diff_b_node)
+    #         # Apply update to b
+    #         b_layer.remove(diff_b_node)
