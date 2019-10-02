5e2752ff3ea6d8008f63c0d09308bd34905c6d51
Luke Tuthill <lukemtuthill@gmail.com>
2019-10-02 16:07:54 +1000

    Fixed imports for merge

--- src/credo/models.py
+++ src/credo/models.py
@@ -1,7 +1,7 @@
 import os
 from django.db import models
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 from django.dispatch import receiver
 from utils.mei.mei_transformer import MeiTransformer
 
