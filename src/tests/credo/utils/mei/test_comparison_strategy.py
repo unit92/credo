5404cf2517b90310975a7662d2d480337f739977
Alex Mirrington <alexmirrington@gmail.com>
2019-10-05 23:57:00 +1000

    Add comparison strategy tests

--- src/tests/credo/utils/mei/test_comparison_strategy.py
+++ src/tests/credo/utils/mei/test_comparison_strategy.py
@@ -1,10 +1,20 @@
 from unittest import TestCase, main
+from credo.utils.mei.comparison_strategy import ComparisonStrategy
 
 
 class TestComparisonStrategy(TestCase):
 
     def setUp(self):
         return
+
+    def test_is_abstract(self):
+        """Ensure the ComparisonStrategy class is abstract
+
+        Assert that trying to create an instance of the ComparisonStrategy
+        class raises an error.
+        """
+        with self.assertRaises(TypeError):
+            ComparisonStrategy()
 
 
 if __name__ == '__main__':
