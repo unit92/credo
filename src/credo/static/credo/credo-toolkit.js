dcf4e87ccb159e20b9d85aac0b9eccfb9678ffd5
Luke Tuthill <lukemtuthill@gmail.com>
2019-10-09 16:56:15 +1100

    Restyle colourscheme

--- src/credo/static/credo/credo-toolkit.js
+++ src/credo/static/credo/credo-toolkit.js
@@ -31,7 +31,7 @@ class CredoToolkit {
   // Sourced from https://material.io/resources/icons/
   commentSvg = `
     <svg class="tooltipped" data-position="bottom" xmlns="http://www.w3.org/2000/svg" width="480" height="480" viewBox="0 0 24 24">
-      <path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM18 14H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z" fill="green" />
+      <path d="M21.99 4c0-1.1-.89-2-1.99-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4-.01-18zM18 14H6v-2h12v2zm0-3H6V9h12v2zm0-3H6V6h12v2z" fill="indigo" />
       <path d="M0 0h24v24H0z" fill="transparent"/>
     </svg>
  `
@@ -502,12 +502,12 @@ class CredoToolkit {
       if (event.target.id === child.id ||
         event.target.parentElement.id === child.id) {
         // make the button look selected
-        child.className = 'waves-effect waves-light btn'
+        child.className = 'waves-effect waves-light btn deep-orange darken-3'
         // set the current tool mode to that button
         this.currentToolMode = event.target.id
       } else {
         // make the button look unselected
-        child.className = 'waves-effect waves-teal btn-flat'
+        child.className = 'waves-effect waves-light btn-flat'
       }
     }
   }
