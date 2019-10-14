d93029894d473b14f289222e48766ff5de55be98
Laith <lmaj3891@uni.sydney.edu.au>
2019-10-14 20:41:37 +1100

    Remove non-relevant code & Fix form errors

--- src/credo/forms.py
+++ src/credo/forms.py
@@ -3,13 +3,11 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(forms.ModelForm):
-    username = forms.CharField(label='username', max_length=100,
-                               required=True, help_text='Test')
-    password = forms.CharField(label='password', required=True,
-                               help_text='PassTest')
-    password_confirm = forms.CharField(label='confirm password',
-                                       required=True)
-    email = forms.EmailField(max_length=254, help_text='Required Test')
+    username = forms.CharField(label='Username', max_length=100)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Confirm Password',
+                                       widget=forms.PasswordInput)
+    email = forms.EmailField(label='Email', max_length=254)
 
     class Meta:
         model = User
@@ -17,7 +15,8 @@ class SignUpForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super().clean()
-        password = cleaned_data.get("username")
-        password_confirmation = cleaned_data.get("password_confirmation")
+        password = cleaned_data.get("password")
+        password_confirmation = cleaned_data.get("password_confirm")
+
         if(password != password_confirmation):
             raise forms.ValidationError("Passwords do not match")
--- src/credo/views.py
+++ src/credo/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse, \
                         HttpResponseBadRequest, \
                         HttpResponseForbidden, \
                         JsonResponse
-from django.contrib.auth import authenticate
 from django.shortcuts import redirect, render
 from django.views import View
 from django.views.decorators.http import require_http_methods
@@ -214,11 +213,9 @@ def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
+            # This code will later contain a section to authenticate and
+            # make user login upon signup
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
             return redirect('index')
     else:
         form = SignUpForm()
